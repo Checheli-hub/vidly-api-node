651e3bed0c9c059dadab56e7b57511acdfa22c89
checheli <mercifulservanta@gmail.com>
2026-03-05 15:43:54 -0800

    Update db.js to use Render MongoDB URI

--- startup/db.js
+++ startup/db.js
@@ -1,10 +1,9 @@
-// startup/db.js
 const mongoose = require("mongoose");
+const config = require("config");
 
-module.exports = function () {
-  const db = process.env.MONGODB_URI || "mongodb://localhost/vidly";
-  mongoose
-    .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log("Connected to MongoDB..."))
-    .catch((err) => console.error("Could not connect to MongoDB...", err));
-};
+const db = process.env.MONGODB_URI || config.get("db"); // Use environment variable first
+
+mongoose
+  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log(`Connected to MongoDB: ${db}`))
+  .catch((err) => console.error("Could not connect to MongoDB...", err));
