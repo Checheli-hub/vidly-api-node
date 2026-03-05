06c3ae56b770173fbc1e7e9ee46d8c67b375209b
checheli <mercifulservanta@gmail.com>
2026-03-05 13:59:42 -0800

    Use Render MongoDB URI

--- startup/db.js
+++ startup/db.js
@@ -1,9 +1,8 @@
-const winston = require('winston');
-const mongoose = require('mongoose');
-const config = require('config');
+const mongoose = require("mongoose");
 
-module.exports = function() {
-  const db = config.get('db');
+module.exports = function () {
+  const db = process.env.MONGODB_URI || "mongodb://localhost/vidly"; // fallback for local dev
   mongoose.connect(db)
-    .then(() => winston.info(`Connected to ${db}...`));
-}
+    .then(() => console.log("Connected to MongoDB..."))
+    .catch((err) => console.error("Could not connect to MongoDB...", err));
+};
