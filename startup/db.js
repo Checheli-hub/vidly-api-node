79cb39e75e0a9f70f8f35d6c8e1793b6b621c800
checheli <mercifulservanta@gmail.com>
2026-03-05 14:18:25 -0800

    Use Render MongoDB URI for deployment

--- startup/db.js
+++ startup/db.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose");
 
 module.exports = function () {
-  // Use Render's MongoDB URI if available, fallback to localhost for local dev
   const db = process.env.MONGODB_URI || "mongodb://localhost/vidly";
-  mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+  mongoose
+    .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log("Connected to MongoDB..."))
     .catch((err) => console.error("Could not connect to MongoDB...", err));
 };
