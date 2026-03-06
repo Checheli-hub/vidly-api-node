e80f740848ea17ad6c1c11f71535de0b4026f596
checheli <mercifulservanta@gmail.com>
2026-03-05 16:17:19 -0800

    Fix startup modules for Render

--- startup/config.js
+++ startup/config.js
@@ -1,3 +1,7 @@
-const config = require("config"); 
-// If you just need config values, you **don’t** export a function:
-module.exports = config;
+const config = require('config');
+
+module.exports = function() {
+  if (!config.get('jwtPrivateKey')) {
+    throw new Error('FATAL ERROR: jwtPrivateKey is not defined.');
+  }
+}
--- startup/db.js
+++ startup/db.js
@@ -2,8 +2,11 @@ const mongoose = require("mongoose");
 const winston = require("winston");
 
 module.exports = function() {
-  const db = process.env.MONGODB_URI || "mongodb://localhost/vidly";
+  const db = process.env.MONGODB_URI; // no fallback to localhost
   mongoose.connect(db)
-    .then(() => winston.info(`Connected to MongoDB...`))
-    .catch(err => winston.error("Could not connect to MongoDB...", err));
+    .then(() => winston.info("Connected to MongoDB..."))
+    .catch(err => {
+      winston.error("Could not connect to MongoDB...", err);
+      process.exit(1); // ensures Render sees the failure
+    });
 };
